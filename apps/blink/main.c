4055839fd0edd7d5308d82f244ec2a056b91a1c2
Justin Lu <justin.lu222@berkeley.edu>
2019-11-14 18:57:39 -0800

    Tried using JLink but could not connect.

--- apps/blink/main.c
+++ apps/blink/main.c
@@ -9,17 +9,22 @@
 #include "nrf_gpio.h"
 
 // Pin definitions for DK
-#define LED NRF_GPIO_PIN_MAP(0,7)
+#define LED NRF_GPIO_PIN_MAP(0,17)
+
+// Test pin 15
+#define TEST NRF_GPIO_PIN_MAP(0,15)
 
 int main(void) {
 
   // Initialize.
   nrf_gpio_cfg_output(LED);
+  nrf_gpio_cfg_output(TEST);
 
   // Enter main loop.
   while (1) {
     // nrf_gpio_pin_set(LED);
     nrf_gpio_pin_toggle(LED);
+    nrf_gpio_pin_toggle(TEST);
     nrf_delay_ms(500);
   }
 }
