77fe7a3e4b69e1d01d4c00d114fb4c50d105eb77
Justin Lu <justin.lu222@berkeley.edu>
2019-11-15 10:26:01 -0800

    JLink flashed once but no pins toggle. Then JLink out of sink.

--- apps/blink/main.c
+++ apps/blink/main.c
@@ -9,22 +9,46 @@
 #include "nrf_gpio.h"
 
 // Pin definitions for DK
-#define LED NRF_GPIO_PIN_MAP(0,17)
+#define LED NRF_GPIO_PIN_MAP(0,7)
 
 // Test pin 15
 #define TEST NRF_GPIO_PIN_MAP(0,15)
+#define TEST1 NRF_GPIO_PIN_MAP(0,14)
+#define TEST2 NRF_GPIO_PIN_MAP(0,13)
+#define TEST3 NRF_GPIO_PIN_MAP(0,12)
+#define TEST4 NRF_GPIO_PIN_MAP(0,11)
+#define TEST16 NRF_GPIO_PIN_MAP(0,16)
+#define TEST17 NRF_GPIO_PIN_MAP(0,17)
+#define TEST18 NRF_GPIO_PIN_MAP(0,18)
+#define TEST19 NRF_GPIO_PIN_MAP(0,19)
 
 int main(void) {
 
   // Initialize.
   nrf_gpio_cfg_output(LED);
   nrf_gpio_cfg_output(TEST);
+  nrf_gpio_cfg_output(TEST1);
+  nrf_gpio_cfg_output(TEST2);
+  nrf_gpio_cfg_output(TEST3);
+  nrf_gpio_cfg_output(TEST4);
+  nrf_gpio_cfg_output(TEST16);
+  nrf_gpio_cfg_output(TEST17);
+  nrf_gpio_cfg_output(TEST18);
+  nrf_gpio_cfg_output(TEST19);
 
   // Enter main loop.
   while (1) {
     // nrf_gpio_pin_set(LED);
     nrf_gpio_pin_toggle(LED);
     nrf_gpio_pin_toggle(TEST);
-    nrf_delay_ms(500);
+    nrf_gpio_pin_toggle(TEST1);
+    nrf_gpio_pin_toggle(TEST2);
+    nrf_gpio_pin_toggle(TEST3);
+    nrf_gpio_pin_toggle(TEST4);
+    nrf_gpio_pin_toggle(TEST16);
+    nrf_gpio_pin_toggle(TEST17);
+    nrf_gpio_pin_toggle(TEST18);
+    nrf_gpio_pin_toggle(TEST19);
+    nrf_delay_ms(250);
   }
 }
